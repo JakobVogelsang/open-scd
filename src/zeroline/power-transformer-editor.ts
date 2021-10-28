2a31b5a362b46723cc46164de86aeb285e073a83
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2021-10-28 21:41:35 +0200

    refactor(power-transformer-editor): adapt to action-icon changes

--- src/zeroline/power-transformer-editor.ts
+++ src/zeroline/power-transformer-editor.ts
@@ -1,36 +1,48 @@
-import { customElement, html, LitElement, property, TemplateResult } from "lit-element";
-import { newWizardEvent } from "../foundation.js";
-import { circuitBreakerIcon, voltageTransformerIcon } from "../icons.js";
-import { wizards } from "../wizards/wizard-library.js";
+import {
+  customElement,
+  html,
+  LitElement,
+  property,
+  TemplateResult,
+} from 'lit-element';
+
+import { newWizardEvent } from '../foundation.js';
+import { voltageTransformerIcon } from '../icons.js';
+
+import { wizards } from '../wizards/wizard-library.js';
 
 @customElement('power-transformer-editor')
 export class PowerTransformer extends LitElement {
-    @property({ type: Element })
-    element!: Element;
+  @property({ type: Element })
+  element!: Element;
 
-    @property({ type: Boolean })
-    readonly = false;
+  @property({ type: Boolean })
+  readonly = false;
 
-    @property({ type: String })
-    get name(): string {
-        return this.element.getAttribute('name') ?? '';
-    }
+  @property({ type: String })
+  get name(): string {
+    return this.element.getAttribute('name') ?? '';
+  }
 
-    onEditWizard(): void {
-        const wizard = wizards['PowerTransformer'].edit(this.element);
-        if (wizard) this.dispatchEvent(newWizardEvent(wizard));
-    }
+  onEditWizard(): void {
+    const wizard = wizards['PowerTransformer'].edit(this.element);
+    if (wizard) this.dispatchEvent(newWizardEvent(wizard));
+  }
 
-    render(): TemplateResult {
-        return html`
-           <action-icon name=${this.name}>
-            ${voltageTransformerIcon}
-            <mwc-fab slot="action" mini icon="edit" @click=${() => this.onEditWizard()}></mwc-fab>
-            <mwc-fab slot="action" mini icon="delete"></mwc-fab>
-            <mwc-fab slot="action" mini icon="account_tree"></mwc-fab>
-            <mwc-fab slot="action" mini icon="forward"></mwc-fab>
-           </action-icon>
-        `;
-    }
-
+  render(): TemplateResult {
+    return html`
+      <action-icon name=${this.name}>
+        ${voltageTransformerIcon}
+        <mwc-fab
+          slot="action"
+          mini
+          icon="edit"
+          @click=${() => this.onEditWizard()}
+        ></mwc-fab>
+        <mwc-fab slot="action" mini icon="delete"></mwc-fab>
+        <mwc-fab slot="action" mini icon="account_tree"></mwc-fab>
+        <mwc-fab slot="action" mini icon="forward"></mwc-fab>
+      </action-icon>
+    `;
+  }
 }
