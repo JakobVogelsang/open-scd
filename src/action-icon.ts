5b18e93bff244a02546b1da495c86d432d53558c
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2021-10-28 21:49:09 +0200

    refactor(action-icon): styles render method

--- src/action-icon.ts
+++ src/action-icon.ts
@@ -4,62 +4,75 @@ import {
   html,
   LitElement,
   property,
-  state,
   TemplateResult,
 } from 'lit-element';
 import { classMap } from 'lit-html/directives/class-map';
 
-
+/** An interactive icon with slotted actions*/
 @customElement('action-icon')
 export class ActionIcon extends LitElement {
-  @property({type: String})
-  icon? : string;
+  /** icon font */
+  @property({ type: String })
+  icon?: string;
 
+  /** Whether size is set to 50px instead of the default 68px*/
   @property({ type: Boolean })
   small = false;
 
-  @property({type: String})
-  name? : string;
+  /** Icon name in the footer */
+  @property({ type: String })
+  name?: string;
 
-  @property({type: Boolean})
+  /** Whether to clip overflowing name at the beginning  */
+  @property({ type: Boolean })
   endEllipsis = false;
 
   private renderIcon(): TemplateResult {
-      if(this.icon) return html`<mwc-icon class="icon ${classMap({small: this.small})}">${this.icon}</mwc-icon>`;
-      return html`<slot></slot>`;
+    if (this.icon)
+      return html`<mwc-icon id="icon" class=${classMap({ small: this.small })}
+        >${this.icon}</mwc-icon
+      >`;
+    return html`<slot
+      id="icon"
+      class=${classMap({ small: this.small })}
+    ></slot>`;
   }
 
-  protected override render(): TemplateResult {
-    return html`<div class="container ${classMap({small: this.small})}" tabindex="0">
-      ${this.renderIcon()}
-      <slot name="action"></slot>
-    </div>
-    <h4 class="footer ${this.endEllipsis}">${this.name}</h4>`;
+  protected render(): TemplateResult {
+    return html`<div
+        id="container"
+        class=${classMap({ small: this.small })}
+        tabindex="0"
+      >
+        ${this.renderIcon()}
+        <slot name="action"></slot>
+      </div>
+      <h4 id="footer" class=${this.endEllipsis}>${this.name}</h4>`;
   }
 
   static styles = css`
-    .container {
+    #container {
       color: var(--mdc-theme-on-surface);
       margin: auto;
       position: relative;
       transition: all 200ms linear;
     }
 
-    .container:focus {
+    #container:focus {
       outline: none;
     }
 
-    .container {
+    #container {
       width: 64px;
       height: 64px;
     }
 
-    .container.small {
+    #container.small {
       width: 50px;
       height: 50px;
     }
 
-    .icon {
+    #icon {
       color: var(--mdc-theme-on-surface);
       transition: transform 150ms linear, box-shadow 200ms linear;
       outline-color: var(--mdc-theme-primary);
@@ -67,59 +80,61 @@ export class ActionIcon extends LitElement {
       outline-width: 0px;
     }
 
-    .icon {
+    #icon {
       --mdc-icon-size: 64px;
     }
 
-    .icon.small {
+    #icon.small {
       --mdc-icon-size: 50px;
     }
 
-    .container:hover > .icon {
+    #container:hover > #icon,
+    #container:hover > ::slotted(:not([slot])) {
       outline: 2px dashed var(--mdc-theme-primary);
       transition: transform 200ms linear, box-shadow 250ms linear;
     }
 
-    .container:focus-within > .icon {
+    #container:focus-within > #icon,
+    #container:focus-within > ::slotted(:not([slot])) {
       outline: 2px solid var(--mdc-theme-primary);
       background: var(--mdc-theme-on-primary);
       transform: scale(0.8);
       transition: transform 200ms linear, box-shadow 250ms linear;
     }
 
-    .container:focus-within > .icon + ::slotted(mwc-fab:nth-child(1)) {
-          transform: translate(0px, -52px);
-        }
-    
-    .container:focus-within > .icon + ::slotted(mwc-fab:nth-child(2)) {
-          transform: translate(0px, 52px);
-        }
-    
-    .container:focus-within > .icon  + ::slotted(mwc-fab:nth-child(3)) {
-          transform: translate(52px, 0px);
-        }
-    
-    .container:focus-within > .icon  + ::slotted(mwc-fab:nth-child(4)) {
-          transform: translate(-52px, 0px);
-        }
+    #container:focus-within > #icon + ::slotted(mwc-fab:nth-of-type(1)) {
+      transform: translate(0px, -52px);
+    }
 
-    .container:focus-within > .icon + ::slotted(mwc-fab:nth-child(5)){
-          transform: translate(52px, -52px);
-        }
-    
-    .container:focus-within > .icon  + ::slotted(mwc-fab:nth-child(6)) {
-          transform: translate(-52px, 52px);
-        }
-    
-    .container:focus-within > .icon  + ::slotted(mwc-fab:nth-child(7)) {
-          transform: translate(-52px, -52px);
-        }
-    
-    .container:focus-within > .icon  + ::slotted(mwc-fab:nth-child(8)) {
-          transform: translate(52px, 52px);
-        }
+    #container:focus-within > #icon + ::slotted(mwc-fab:nth-of-type(2)) {
+      transform: translate(0px, 52px);
+    }
 
-    .footer {
+    #container:focus-within > #icon + ::slotted(mwc-fab:nth-of-type(3)) {
+      transform: translate(52px, 0px);
+    }
+
+    #container:focus-within > #icon + ::slotted(mwc-fab:nth-of-type(4)) {
+      transform: translate(-52px, 0px);
+    }
+
+    #container:focus-within > #icon + ::slotted(mwc-fab:nth-of-type(5)) {
+      transform: translate(52px, -52px);
+    }
+
+    #container:focus-within > #icon + ::slotted(mwc-fab:nth-of-type(6)) {
+      transform: translate(-52px, 52px);
+    }
+
+    #container:focus-within > #icon + ::slotted(mwc-fab:nth-of-type(7)) {
+      transform: translate(-52px, -52px);
+    }
+
+    #container:focus-within > #icon + ::slotted(mwc-fab:nth-of-type(8)) {
+      transform: translate(52px, 52px);
+    }
+
+    #footer {
       color: var(--mdc-theme-on-surface);
       font-family: 'Roboto', sans-serif;
       font-weight: 300;
@@ -132,7 +147,7 @@ export class ActionIcon extends LitElement {
       text-align: center;
     }
 
-    .footer.right {
+    #footer.right {
       direction: rtl;
     }
 
@@ -146,22 +161,25 @@ export class ActionIcon extends LitElement {
       opacity: 0;
     }
 
-    .icon + ::slotted(mwc-fab) {
+    #icon + ::slotted(mwc-fab) {
       top: 8px;
       left: 8px;
     }
 
-    .icon.small + ::slotted(mwc-fab) {
+    #icon.small + ::slotted(mwc-fab) {
       top: 2px;
       left: 2px;
     }
 
-    .container:focus-within > ::slotted(mwc-fab) {
+    #container:focus-within > ::slotted(mwc-fab) {
       transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1),
         opacity 250ms linear;
       pointer-events: auto;
       opacity: 1;
     }
 
+    slot:not([name]) {
+      pointer-events: none;
+    }
   `;
 }
